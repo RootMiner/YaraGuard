9a67f0ee72826738f6c459c985292d20da67b6a0
MY7H <100413372+MY7H404@users.noreply.github.com>
2023-12-08 01:42:36 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,26 +2,25 @@ import os
 import yara
 from sys import argv
 from rich import print
-
 try:
     a = 0
     dir_name = './rules/'
     file_to_scan = argv[1]
 
     for file in os.listdir(dir_name):
-        file_path = dir_name + file
-        rules = yara.compile(filepath=file_path)
-        matches = rules.match(file_to_scan)
+            file_path = dir_name + file
 
-        if matches and a == 0:
-            print("\n\n[bold red]       ||>>> Malware Detected <<<||[/bold red]\n\n")
-            print(f"[bold red][+][/bold red] the file [bold green]{file_to_scan}[/bold green] matches the [bold cyan]Yara rules[/bold cyan]:")
-            a = 1
-        if matches:
-            for match in matches:
-                print(f"- Rule: [bold yellow]{match.rule}[/bold yellow]")
+            rules = yara.compile(filepath=file_path)
 
-    if a == 0:
-        print("\n\n[bold green]       ||>>> No Malware Detected <<<||[/bold green]\n\n")
-except Exception as e:
-    print(f"[bold red][*] Something went wrong: {e}[/bold red] \n\n[bold green]python3[/bold green] [bold yellow]boldscan.py[/bold yellow] [bold cyan]{file_to_scan}[/bold cyan]")
+            matches = rules.match(file_to_scan)
+
+
+            if matches and a == 0:
+                print("\n\n[bold red]       ||>>> Malware Detected <<<||[/bold red]\n\n")
+                print(f"[bold red][+][/bold red] the file [bold green]{file_to_scan}[/bold green] matches the [bold cyan]Yara rules[/bold cyan]:")
+                a = 1
+            if matches:
+                for match in matches:
+                    print(f"- Rule: [bold yellow]{match.rule}[/bold yellow]")
+except:
+    print("[bold red][*]something went wrong![/bold red] \n\n[bold green]python3[/bold green] [bold yellow]boldscan.py[/bold yellow] [bold cyan]{file to scan}[/bold cyan]")
